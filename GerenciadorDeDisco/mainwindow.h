340353ba8b2fd2e2672924b8a7f7738f04c917b7
Raí Vitor <raivitor@gmail.com>
2015-06-15 18:53:49 -0300

    mais comentario

--- GerenciadorDeDisco/mainwindow.h
+++ GerenciadorDeDisco/mainwindow.h
@@ -15,19 +15,39 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void AbrirDialog(); // Abre a primeira janela para conf o disco
+    void AbrirDialog();/**< Abre a primeira tela do sistema */ // Abre a primeira janela para conf o disco
     void Plotar(); //Plotar gráfico
     void Listar(); //Lista os arquivos
 
 private slots:
+    /**
+     * Função chamada ao clicar no botão Salvar
+     */
     void on_salvar_clicked();
+
+    /**
+     * Função chamada ao clicar no botão Excluir
+     */
     void on_excluir_clicked();
+
+    /**
+     * Função chamada ao clicar no botão Listar
+     */
     void on_listar_clicked();
 
+    /**
+     * Função chamada ao clicar no botão Formatar
+     */
     void on_formatar_clicked();
 
+    /**
+     * Função chamada ao clicar no botão Buscar
+     */
     void on_buscar_clicked();
 
+    /**
+     * Função chamada ao clicar no botão Desfragmentar
+     */
     void on_desfragmentar_clicked();
 
 private:
